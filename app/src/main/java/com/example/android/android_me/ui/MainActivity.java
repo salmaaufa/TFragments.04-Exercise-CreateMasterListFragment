0080c9f4aa1853786e51a269f3f9eb7885c8e7d7
salmaaufa <salma_aufadina_25rpl@student.smktelkom-mlg.sch.id>
2018-03-16 21:56:19 +0700

    Display the static master list fragment

---
+++ app/src/main/java/com/example/android/android_me/ui/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.android.android_me.ui;
+
+/**
+ * Created by Asus on 16/03/2018.
+ */
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.example.android.android_me.R;
+
+// This activity is responsible for displaying the master list of all images
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+
+}
